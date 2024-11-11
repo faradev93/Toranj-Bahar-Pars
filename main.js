5628b68f316f93e4ee44d2550e966c2dc7617c63
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-11-11 18:03:17 +0330

    Container Tamam

--- main.js
+++ main.js
@@ -1,50 +1,11 @@
 import "./style.css";
+import "./style-slider-bird.css";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+// Keen Slider
+import "keen-slider/keen-slider.min.css";
+import KeenSlider from "keen-slider";
+//
 
-// setupCounter(document.querySelector('#counter'))
-
-
-
-
-// let second = 0;
-// let minute = 0;
-// const clock = setInterval(() => {
-//   if (second === 59) {
-//     second = 0;
-//     minute += 1;
-//   } else {
-//     second += 1;
-//   }
-//   console.log(`${minute}:${second}`);
-//   if (minute === 3) {
-//     clearInterval(clock);
-//   }
-// }, 1000);
-
-
-
-
-
-
-
-// begin Project
